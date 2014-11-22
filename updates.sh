d9e812db30471a17e6507e9b5b2ff77945794381
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-11-22 12:17:20 +0200

    Remove all cherries

--- updates.sh
+++ updates.sh
@@ -2,21 +2,6 @@
 
 android=${PWD}
 
-# sensorservice: Add legacy sensors fusion.
-cherries+=(59311)
-
-# vold: allow swapped storage paths at vold init
-cherries+=(56515)
-
-# Advanced storage: vold swap storage paths
-cherries+=(56516)
-
-# storage: allow swapping primary storage for apps
-cherries+=(56950)
-
-# Advanced storage: swap primary storage for apps
-cherries+=(56951)
-
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
