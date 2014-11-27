7ec52f6f21395f1a4a1c53b8ccd3a2d09b970a6b
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-11-27 12:48:27 +0200

    Add API in libnetutils to fix cnd blob

--- updates.sh
+++ updates.sh
@@ -5,6 +5,9 @@ android=${PWD}
 # Revert "Revert "Reenable support for non-PIE executables""
 cherries+=(79136)
 
+# core: Add API to retrieve mtu size
+cherries+=(79526)
+
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
