0dd266a58d8fd1ce0431cb0022171847193e7510
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-12-13 11:52:00 +0200

    Add legacy sensors fusion

--- updates.sh
+++ updates.sh
@@ -5,6 +5,9 @@ android=${PWD}
 # Revert "Revert "Reenable support for non-PIE executables""
 cherries+=(79136)
 
+# sensorservice: Add legacy sensors fusion.
+cherries+=(81684)
+
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
