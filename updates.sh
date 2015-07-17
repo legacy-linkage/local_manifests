53f15e1da893f7a5b54feac22ac217ea4cde76fa
Michael Bestas <mikeioannina@gmail.com>
2015-07-17 16:50:08 +0300

    Add LPA cherries
    Change-Id: I709c88946f1cc9a457c3e50ae831812bc081c224
    
    Conflicts:
    	updates.sh

--- updates.sh
+++ updates.sh
@@ -8,6 +8,27 @@ cherries+=(59311)
 # Revert "audioflinger: fix static track end detection"
 cherries+=(108644)
 
+# audio: Add A2DP notification support
+cherries+=(63131)
+
+# framework/av: Add Usb AoA v2.0 support
+cherries+=(63410)
+
+# libstagefright: Convert mono to stereo for LPA clips
+cherries+=(63411)
+
+# libstagefright: Stability issue with LPA play back.
+cherries+=(63412)
+
+# frameworks/av: Fix playback for LPA audio when using gapless api
+cherries+=(103480)
+
+# libstagefright: Fix LPA default min duration
+cherries+=(103549)
+
+# libstagefright: Disable LPAPlayer verbose logging
+cherries+=(103550)
+
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
