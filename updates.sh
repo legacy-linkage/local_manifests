460cb479b85b80980c7a8adef62355c46ee2a32d
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-09-05 12:44:40 +0300

    Add patch to test audio stuttering fix

--- updates.sh
+++ updates.sh
@@ -5,6 +5,9 @@ android=${PWD}
 # sensorservice: Add legacy sensors fusion.
 cherries+=(59311)
 
+# Revert "audioflinger: fix static track end detection"
+cherries+=(108644)
+
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
