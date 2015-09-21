b8f400792f0840f9b0938684c8b9fe6ac6a5d70d
Michael Bestas <mikeioannina@gmail.com>
2015-09-22 00:16:50 +0300

    Track forked CM repos
    * Now that CM is EOL, there's no point in pushing changes to CM gerrit
    * Fork fw/av to cherry-pick our 720p & LPA changes
    * Fork a few repos to backport the cm-12.1 BFQIO changes
    
    Change-Id: I6e477b767ae308abe33010059409921b5125e229

--- updates.sh
+++ updates.sh
@@ -5,30 +5,6 @@ android=${PWD}
 # sensorservice: Add legacy sensors fusion.
 cherries+=(59311)
 
-# audio: Add A2DP notification support
-cherries+=(63131)
-
-# framework/av: Add Usb AoA v2.0 support
-cherries+=(63410)
-
-# libstagefright: Convert mono to stereo for LPA clips
-cherries+=(63411)
-
-# libstagefright: Stability issue with LPA play back.
-cherries+=(63412)
-
-# frameworks/av: Fix playback for LPA audio when using gapless api
-cherries+=(103480)
-
-# libstagefright: Fix LPA default min duration
-cherries+=(103549)
-
-# libstagefright: Disable LPAPlayer verbose logging
-cherries+=(103550)
-
-# libstagefright: Update LPAPlayer callbacks
-cherries+=(109071)
-
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
