0d1901eeb42833d46c83868c39c11e02b066c827
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-09-22 09:12:57 +0300

    Add patches for LPA

--- updates.sh
+++ updates.sh
@@ -20,6 +20,30 @@ cherries+=(101243)
 # build: Add option to disable block-based ota
 cherries+=(102837)
 
+# libstagefright: Add legacy LPAPlayer
+cherries+=(103507)
+
+# audio: Add A2DP notification support
+cherries+=(103509)
+
+# framework/av: Add Usb AoA v2.0 support
+cherries+=(103510)
+
+# libstagefright: Convert mono to stereo for LPA clips
+cherries+=(103511)
+
+# libstagefright: Stability issue with LPA play back.
+cherries+=(103512)
+
+# frameworks/av: Fix playback for LPA audio when using gapless api
+cherries+=(103513)
+
+# libstagefright: Disable LPAPlayer verbose logging
+cherries+=(103556)
+
+# libstagefright: Update LPAPlayer callbacks
+cherries+=(109096)
+
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
