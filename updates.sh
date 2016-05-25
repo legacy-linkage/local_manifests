2a58deeb32fd2317419a8bf80f6cb7c0d52712b8
Rudolf Tammekivi <rtammekivi@gmail.com>
2016-05-25 16:19:03 +0300

    Add initial cm-13.0 cherries
    From LX

--- updates.sh
+++ updates.sh
@@ -2,6 +2,18 @@
 
 android=${PWD}
 
+# Revert "Revert "Reenable support for non-PIE executables""
+cherries+=(117733)
+
+# arm: Allow disabling PIE for dynamically linked executables
+cherries+=(123032)
+
+# policy: Show a simple dialog about optimizing apps
+cherries+=(131627)
+
+# Barrier only messages shouldn't prevent the idle handlers from running
+cherries+=(131628)
+
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
