99defabd284d1aba5322dafc8305aeec50fcf65a
Rudolf Tammekivi <rtammekivi@gmail.com>
2017-01-05 17:58:00 +0200

    Fork repopick.py and point to review.lineageos.org
    Update the numbers for lineageos gerrit.

--- updates.sh
+++ updates.sh
@@ -3,53 +3,53 @@
 android=${PWD}
 
 # sensorservice: Add legacy sensors fusion.
-cherries+=(59311)
+cherries+=(70355)
 
 # audio: Add A2DP notification support
-cherries+=(63131)
+cherries+=(63465)
 
 # framework/av: Add Usb AoA v2.0 support
-cherries+=(63410)
+cherries+=(63464)
 
 # libstagefright: Convert mono to stereo for LPA clips
-cherries+=(63411)
+cherries+=(63463)
 
 # libstagefright: Stability issue with LPA play back.
-cherries+=(63412)
+cherries+=(63462)
 
 # frameworks/av: Fix playback for LPA audio when using gapless api
-cherries+=(103480)
+cherries+=(63573)
 
 # libstagefright: Fix LPA default min duration
-cherries+=(103549)
+cherries+=(63572)
 
 # libstagefright: Disable LPAPlayer verbose logging
-cherries+=(103550)
+cherries+=(63571)
 
 # libstagefright: Update LPAPlayer callbacks
-cherries+=(109071)
+cherries+=(63478)
 
 # input: Set explicit ioprio
-cherries+=(123930)
+cherries+=(67360)
 
 # surfaceflinger: Set explicit ioprio where required
-cherries+=(123932)
+cherries+=(70229)
 
 # display: Set thread priority using cutils
-cherries+=(123933)
+cherries+=(75249)
 
 # hwc: Set ioprio for vsync thread
-cherries+=(123934)
+cherries+=(75248)
 
 # Set IO scheduler using persist property
-cherries+=(123935)
+cherries+=(130381)
 
 # cutils: Squashed backport of BFQIO cgroups support
-cherries+=(123936)
+cherries+=(143080)
 
 if [ -z $cherries ]
 then
 	echo "Nothing to cherry-pick!"
 else
-	${android}/build/tools/repopick.py -b ${cherries[@]}
+	${android}/.repo/local_manifests/repopick.py -b ${cherries[@]}
 fi
